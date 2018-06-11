acc09b0bac0370fc51611dae7e5b9b1507848572
H/S <kidoairakusyunkasyuutou@gmail.com>
2018-06-12 03:31:31 +0900

    ipaddress change

--- all-start.bat
+++ all-start.bat
@@ -1,2 +1,3 @@
 @echo off
-docker-compose up -d --build
+set ALIAS=Wi-Fi
+powershell "$env:IPAddress=$(Get-NetIPAddress| ? {$_.InterfaceAlias -like $env:ALIAS -and $_.AddressFamily -like 'IPv4'}|select IPAddress)[0].IPAddress;docker-compose up -d --build"
